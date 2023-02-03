9680e1f24fb4edc08e940244e803cabb25ebad61
HadziqM <dimascrazz@gmail.com>
2023-02-03 12:33:09 +0700

    migrate paralel loop

--- src/event/log.rs
+++ src/event/log.rs
@@ -1,6 +1,6 @@
 use std::{path::Path, time::SystemTime};
 
-use serenity::{prelude::Context, model::{prelude::{UserId, ChannelId}, user::User}};
+use serenity::{prelude::Context, model::{prelude::{UserId, ChannelId}, user::User}, builder::CreateMessage};
 
 use crate::{Init,ErrorLog};
 
@@ -15,21 +15,19 @@ pub async fn logging(ctx:&Context,init:&Init,wish:Option<User>){
     let user =UserId(init.discord.author_id).to_user(&ctx.http).await.unwrap();
     let mut err = ErrorLog::new(ctx,init,&user).await;
     let channel = ChannelId(init.log_channel.erupe_channel);
-    if let Err(why)=channel.send_message(&ctx.http,|m|{
-        m.add_file(&path).content(&format!("LOG AT <t:{}:F>",SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs()))
-    }).await{
+    if let Err(why)=channel.send_message(&ctx.http,CreateMessage::new().add_file(&path).content(&format!("LOG AT <t:{}:F>"
+        ,SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs()))).await{
         err.change_error(why.to_string(), "sending log", "failed to send log".to_string());
         err.log_error_channel().await;
     };
     //send dm to wish if servercrash
     if let Some(wish)=wish{
-        if let Err(why)= wish.dm(&ctx.http, |m|{
-            m.content("server crash on about now or few minutes before, please check the log").add_file(&path)
-        }).await{
+        if let Err(why)= wish.dm(&ctx.http,CreateMessage::new()
+                .content("server crash on about now or few minutes before, please check the log").add_file(&path)).await{
             err.change_error(why.to_string(),"dm wish the log File", "please investigate".to_string());
             err.log_error_channel().await;
         }
-        if let Err(why)= channel.send_message(&ctx.http, |m|m.content("SERVER MIGHT CRASH JUST ABOUT NOW")).await{
+        if let Err(why)= channel.send_message(&ctx.http, CreateMessage::new().content("SERVER MIGHT CRASH JUST ABOUT NOW")).await{
             err.change_error(why.to_string(), "sending emergency message", "server crash now".to_string());
             err.log_error_channel().await;
         }
--- src/event/paralel.rs
+++ src/event/paralel.rs
@@ -1,6 +1,6 @@
 use std::time::SystemTime;
 
-use serenity::{prelude::Context, model::{prelude::{ChannelId, Message, UserId}, user::User}, builder::CreateEmbed};
+use serenity::{prelude::Context, model::{prelude::{ChannelId, Message, UserId}, user::User}, builder::{CreateEmbed, EditMessage}};
 use crate::{Init,PgConn,ErrorLog};
 use crate::reusable::postgress::server::Servers;
 use crate::reusable::utils::Color;
@@ -39,11 +39,6 @@ impl<'a> Server<'a> {
         };
         Some(Server{pg,err,msg,ctx})
     }
-    async fn test(&mut self){
-        self.msg.edit(&self.ctx.http, |x|{
-            x.content(&format!("hello edited <t:{}:R>",SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs()))
-        }).await.unwrap();
-    }
     fn build_embed(&self,serv:Vec<Servers>)->CreateEmbed{
         let mut field = Vec::new();
         let mut pc = 0;
@@ -53,16 +48,12 @@ impl<'a> Server<'a> {
             field.push((&x.name,z,true));
             pc += x.cp
         }
-        let mut emb = CreateEmbed::default();
+        let mut emb = CreateEmbed::new();
         emb.title("Server Status").description(&format!("Mhfz Server Status updated <t:{}:R>\nPlayer Count Total = {pc}",SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs()))
         .fields(field).color(Color::Random.throw());
         emb
     }
     async fn edit_msg(&mut self)->i32{
-        if let Err(why)=self.pg.re_connect().await{
-            self.err.pgcon_error_ch(why.to_string(), "reconnect pgcon").await;
-            return 0;
-        }
         let z = match self.pg.get_server().await{
             Ok(x)=>x,
             Err(why)=>{
@@ -72,9 +63,7 @@ impl<'a> Server<'a> {
         };
         let pc  = z.iter().map(|e|e.cp).sum::<i32>();
         let embed = self.build_embed(z);
-        if let Err(why)=self.msg.edit(&self.ctx.http, |m|{
-            m.set_embed(embed)
-        }).await{
+        if let Err(why)=self.msg.edit(&self.ctx.http,EditMessage::new().embed(embed)).await{
             self.err.discord_error(why.to_string(), "editing message paralel loop").await;
         }
         self.pg.close().await;
