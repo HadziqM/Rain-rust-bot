d8a76027b8f0261f92ff28c4bd743c81662745e5
HadziqM <dimascrazz@gmail.com>
2023-02-07 13:20:23 +0700

    add missing file

---
+++ src/reusable/bitwise/mod.rs
@@ -0,0 +1,50 @@
+use std::num::ParseIntError;
+use std::fmt;
+
+pub mod distribution;
+
+//create own error enum
+#[derive(Debug)]
+pub enum BitwiseError {
+    OddLength,
+    ParseInt(ParseIntError),
+    Sqlx(sqlx::Error),
+}
+
+impl fmt::Display for BitwiseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            BitwiseError::OddLength => "input string has an odd number of bytes".fmt(f),
+            BitwiseError::ParseInt(e) => e.fmt(f),
+            BitwiseError::Sqlx(e)=>e.fmt(f)
+        }
+    }
+}
+impl std::error::Error for BitwiseError {}
+
+//implement parser for error
+impl From<ParseIntError> for BitwiseError {
+    fn from(e: ParseIntError) -> Self {
+        BitwiseError::ParseInt(e)
+    }
+}
+impl From<sqlx::Error> for BitwiseError{
+    fn from(value: sqlx::Error) -> Self {
+        BitwiseError::Sqlx(value)
+    }
+}
+
+pub struct Bitwise;
+
+impl Bitwise{
+    pub fn decode(hex_value:&str)->Result<Vec<u8>,BitwiseError>{
+        if hex_value.len()%2 != 0{
+            return Err(BitwiseError::OddLength);
+        }
+        //pair it two then decode
+        (0..hex_value.len())
+            .step_by(2)
+            .map(|i| u8::from_str_radix(&hex_value[i..i + 2], 16).map_err(|e| e.into()))
+            .collect()
+    }
+}
