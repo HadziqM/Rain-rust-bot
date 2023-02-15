37671817526d6c3bb79d926ce7dbd4abdd04f7e7
HadziqM <dimascrazz@gmail.com>
2023-02-15 21:37:49 +0700

    add jp translation

--- src/reusable/component/mod.rs
+++ src/reusable/component/mod.rs
@@ -4,5 +4,45 @@ pub mod guide;
 pub mod card;
 pub mod registered;
 pub mod discord;
+pub mod json;
 
 pub struct Components;
+
+#[derive(Debug)]
+pub enum MyErr{
+    Serenity(serenity::Error),
+    Tokio(tokio::io::Error),
+    Utf8(std::str::Utf8Error),
+    Serde(serde_json::Error)
+}
+impl std::error::Error for MyErr {}
+impl std::fmt::Display for MyErr{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self{
+            MyErr::Tokio(x)=>x.fmt(f),
+            MyErr::Serenity(x)=>x.fmt(f),
+            MyErr::Utf8(x)=>x.fmt(f),
+            MyErr::Serde(x)=>x.fmt(f)
+        }
+    }
+}
+impl From<tokio::io::Error> for MyErr{
+    fn from(value: tokio::io::Error) -> Self {
+        MyErr::Tokio(value)
+    }
+}
+impl From<serenity::Error> for MyErr {
+    fn from(value: serenity::Error) -> Self {
+        MyErr::Serenity(value)
+    }
+}
+impl From<std::str::Utf8Error> for MyErr{
+    fn from(value: std::str::Utf8Error) -> Self {
+        MyErr::Utf8(value)
+    }
+}
+impl From<serde_json::Error> for MyErr {
+    fn from(value: serde_json::Error) -> Self {
+        MyErr::Serde(value)
+    }
+}
